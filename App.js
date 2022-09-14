393239d642b77f9005d31aa1c909a49193e8d53b
Micky <michaelgozo4@gmail.com>
2022-09-14 15:44:11 +0000

    images

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.carContainer}>
+
+        <ImageBackground
+        source={require('./assets/images/ModelX.jpeg')}
+        style={styles.image}
+        />
+
+        <View style={style.titles}>
+          <Text style={style.title}>Model S</Text>
+          <Text style={style.subtitle}>Starting at $69,000</Text>
+        </View>
+
+      </View>
+
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -17,4 +30,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  carContainer: {
+    width: "100%",
+    height: '100%',
+  },
+
+  titles: {
+    marginTop: '30%',
+    width: "100%",
+    alignItems: 'center',
+  },
+
+  title: {
+    fontSize: 40,
+    fontWeight: '600',
+  },
+
+  subtitle: {
+    fontSize: 16,
+    color: '#5c5e62',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  }
 });
